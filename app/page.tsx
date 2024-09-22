8c42da0904b4c49fb34c9b7d8f10685661d0947e
PoundforPound <a.zenit2014@yandex.ru>
2024-09-22 13:53:13 +0300

    add TopBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Categories, Container, SortPopup, Title, TopBar } from '@/components/shared';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    
-<h1>
-  Hello World
-</h1>
-    
+    <>
+      <Container className="mt-10">
+        <Title text="All pizzas" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+      {/* <div style={{ height: '3000px' }} /> */}
+    </>
   );
 }
