a8868814920381739084885d039a29d4c3ef981f
PoundforPound <a.zenit2014@yandex.ru>
2024-09-22 17:19:34 +0300

    add 1filterparametr and customize checkbox

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Categories, Container, SortPopup, Title, TopBar } from '@/components/shared';
+import { Categories, Container, Filters, SortPopup, Title, TopBar } from '@/components/shared';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
@@ -10,6 +10,18 @@ export default function Home() {
       </Container>
       <TopBar />
       {/* <div style={{ height: '3000px' }} /> */}
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          {/*Filtration*/}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+          {/*Item List */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">Item List</div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
