ea8309ba368007bd48f6f170f53690a06cae40f3
poundforpound <a.zenit2014@yandex.ru>
2024-09-25 17:04:50 +0300

    add product-card and product-group-list

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
-import { Categories, Container, Filters, SortPopup, Title, TopBar } from '@/components/shared';
+import {
+  Categories,
+  Container,
+  Filters,
+  ProductCard,
+  ProductsGroupList,
+  SortPopup,
+  Title,
+  TopBar,
+} from '@/components/shared';
 import { Try } from '@/components/shared/try';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
@@ -12,14 +21,75 @@ export default function Home() {
       <TopBar />
       {/* <div style={{ height: '3000px' }} /> */}
       <Container className="mt-10 pb-14">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[80px]">
           {/*Filtration*/}
           <div className="w-[250px]">
             <Filters />
           </div>
           {/*Item List */}
           <div className="flex-1">
-            <div className="flex flex-col gap-16">Item List</div>
+            <div className="flex flex-col gap-16">
+              <ProductsGroupList
+                title="Pizzas"
+                items={[
+                  {
+                    id: 1,
+                    name: 'Cheesburger pizza',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:233x233/11EE7D61698827EE9B8DB6D0AEC53410.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 1,
+                    name: 'Cheesburger pizza',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:233x233/11EE7D61698827EE9B8DB6D0AEC53410.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 1,
+                    name: 'Cheesburger pizza',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:233x233/11EE7D61698827EE9B8DB6D0AEC53410.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+                categoryId={1}
+              />
+              <ProductsGroupList
+                title="Breakfast"
+                items={[
+                  {
+                    id: 1,
+                    name: 'Cheesburger pizza',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:233x233/11EE7D61698827EE9B8DB6D0AEC53410.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 1,
+                    name: 'Cheesburger pizza',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:233x233/11EE7D61698827EE9B8DB6D0AEC53410.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 1,
+                    name: 'Cheesburger pizza',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:233x233/11EE7D61698827EE9B8DB6D0AEC53410.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+                categoryId={2}
+              />
+            </div>
           </div>
         </div>
       </Container>
